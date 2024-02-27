3c0decf2210156759c8cfe48eb6978edc2da8dec
Andreas Bergman <andreas@arjancodes.com>
2024-02-27 15:13:30 +0100

    Add example for pure and not pure function

---
+++ mastering_the_tools/the_mighty_function/is_pure.py
@@ -0,0 +1,17 @@
+from typing import Callable
+
+def G(a: int, b: int) -> int:
+    return a + b
+
+
+def F(func: Callable[[int, int], int]) -> None:
+    func(1, 2)
+
+
+def main() -> None:
+
+    F(G)
+
+
+if __name__ == "__main__":
+    main()
