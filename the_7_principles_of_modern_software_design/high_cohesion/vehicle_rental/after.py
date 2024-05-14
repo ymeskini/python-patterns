a10f834039d2eb3ba8ff899fffda5249d440f513
Andreas Bergman <andreas@arjancodes.com>
2024-05-14 11:25:32 +0200

    Refactor: Remove global coupling for high cohesion rental example

--- the_7_principles_of_modern_software_design/high_cohesion/vehicle_rental/after.py
+++ the_7_principles_of_modern_software_design/high_cohesion/vehicle_rental/after.py
@@ -1,4 +1,3 @@
-
 from dataclasses import dataclass
 
 
@@ -7,19 +6,20 @@ class VehicleData:
     price_per_km: int
     price_per_day: int
 
+
 RENT_VEHICLE_DATA = {
     "vw": VehicleData(price_per_km=30, price_per_day=6000),
-    "bmw": VehicleData(price_per_km = 35, price_per_day = 8500),
-    "ford": VehicleData(price_per_km = 25, price_per_day = 12000),
+    "bmw": VehicleData(price_per_km=35, price_per_day=8500),
+    "ford": VehicleData(price_per_km=25, price_per_day=12000),
 }
 
 
-def read_vehicle_type() -> str:
+def read_vehicle_type(rentable_types: dict[str, VehicleData]) -> str:
     """Reads the vehicle type from the user."""
     vehicle_type = ""
-    while vehicle_type not in RENT_VEHICLE_DATA:
+    while vehicle_type not in rentable_types:
         vehicle_type = input(
-            f"What type of vehicle would you like to rent ({', '.join(RENT_VEHICLE_DATA)})? "
+            f"What type of vehicle would you like to rent ({', '.join(rentable_types)})? "
         )
     return vehicle_type
 
@@ -52,9 +52,11 @@ def read_kms_to_drive() -> int:
     return km
 
 
-def compute_rental_cost(vehicle_type: str, days: int, km: int) -> int:
+def compute_rental_cost(
+    vehicle_type: str, days: int, km: int, rentable_types: dict[str, VehicleData]
+) -> int:
     """Computes the rental cost for a vehicle."""
-    vehicle_data = RENT_VEHICLE_DATA[vehicle_type]
+    vehicle_data = rentable_types[vehicle_type]
     paid_kms = max(km - 100, 0)
     return (vehicle_data.price_per_km * paid_kms) + (vehicle_data.price_per_day * days)
 
@@ -62,14 +64,14 @@ def compute_rental_cost(vehicle_type: str, days: int, km: int) -> int:
 def main() -> None:
     print("Vehicle Rental before")
 
-    vehicle_type = read_vehicle_type()
+    vehicle_type = read_vehicle_type(RENT_VEHICLE_DATA)
 
     days = read_rent_days()
 
     km = read_kms_to_drive()
 
     # compute the final rental price
-    rental_price = compute_rental_cost(vehicle_type, days, km)
+    rental_price = compute_rental_cost(vehicle_type, days, km, RENT_VEHICLE_DATA)
 
     # print the result
     print(f"The total price of the rental is ${(rental_price / 100):.2f}")
