055c6feeca907e67ecb96c11ebcbd58632c6dbb4
Andreas Bergman <andreas@arjancodes.com>
2024-07-19 13:27:46 +0300

    Update example for error handling to inlucde different types of imports

--- setting_up_a_complex_software_project/modules_and_packages/package_1/file_1.py
+++ setting_up_a_complex_software_project/modules_and_packages/package_1/file_1.py
@@ -5,3 +5,7 @@ from package_3.sub_package_1 import file_5
 def file_1_function():
     print("package_1.file_1_function")
     print(file_5.__name__)
+
+
+def duplicate_function():
+    print("package_1.file_1.duplicated_function")
---
+++ setting_up_a_complex_software_project/modules_and_packages/specific_import.py
@@ -0,0 +1,3 @@
+from module import VOWELS
+from package_1 import file_1
+from package_1.file_1 import file_1_function
