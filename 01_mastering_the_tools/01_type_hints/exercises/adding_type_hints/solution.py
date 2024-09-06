8756b85d71aeb2a0a40cb5cc8a3dc9c038886a6e
Andreas Bergman <andreas@arjancodes.com>
2024-09-06 16:57:31 +0200

    fix: Add missing function to the solution for type hints

--- 01_mastering_the_tools/01_type_hints/exercises/adding_type_hints/solution.py
+++ 01_mastering_the_tools/01_type_hints/exercises/adding_type_hints/solution.py
@@ -1,18 +1,31 @@
 def add_book(collection: list[dict[str, str]], title: str, author: str) -> None:
     collection.append({"title": title, "author": author})
 
+
 def display_books(collection: list[dict[str, str]]) -> None:
     for book in collection:
         print(f"Title: {book['title']}, Author: {book['author']}")
 
+
+def find_book_by_title(
+    collection: list[dict[str, str]], title: str
+) -> dict[str, str] | None:
+    for book in collection:
+        if book["title"] == title:
+            return book
+    return None
+
+
 def filter_even_numbers(numbers: list[int]) -> list[int]:
     return [num for num in numbers if num % 2 == 0]
+
 
 def main() -> None:
     my_books: list[dict[str, str]] = []
     add_book(my_books, "1984", "George Orwell")
     add_book(my_books, "To Kill a Mockingbird", "Harper Lee")
     display_books(my_books)
+
 
 if __name__ == "__main__":
     main()
